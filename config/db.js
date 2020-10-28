48c0d4f2053c2c71b1c72a9239dad57c3f499323
Alvaro <alvarucp@gmail.com>
2020-10-28 19:08:58 +0100

    fixes

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  console.log('Intentando conectar con la DB');
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/blabla', {
       useNewUrlParser: true,
@@ -9,7 +10,7 @@ const connectDB = async () => {
     });
     console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold);
   } catch (err) {
-    console.log(`Error: ${err.message}`.red);
+    console.log(`Error al conectar con la DB: ${err.message}`.red);
     process.exit(1);
   }
 };
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const cors = require('cors');
 const connectDB = require('./config/db');
 dotenv.config({ path: './config/config.env' });
@@ -15,9 +15,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
+// if (process.env.NODE_ENV === 'development') {
+//   app.use(morgan('dev'));
+// }
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
@@ -25,4 +25,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use('/api/transactions', transactionsRoutes);
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+  console.log('MONGODB VAR', process.env.MONGO_URI);
+});
